dd8eb844f97873dafda177cd39802f7826e19d3a
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-12-13 03:56:23 +0300

    user routes & controllers added

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { addAddress, addToWishlist, deleteAddress, getAddresses, getWishlist, removeFromWishlist, updateAddress } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router.use(protectRoute);
+
+router.post("/addresses", addAddress);
+router.get("/addresses", getAddresses);
+router.put("/addresses/:addressId", updateAddress);
+router.delete("/addresses/:addressId", deleteAddress);
+
+router.post("/wishlist", addToWishlist);
+router.delete("/wishlist/:productId", removeFromWishlist);
+router.get("/wishlist", getWishlist);
+
+export default router;
